6654cb7a0eb69f31dbe811243b844798f4be8fc2
Kevin Pereda <kevinpereda125@meta.com>
2025-06-06 17:05:30 -0700

    Added all the cover arts for the playlists

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -3,7 +3,7 @@ const data = [
     "playlistID": 1,
     "playlist_name": "Bedroom Pop Vibes",
     "playlist_author": "Beast Boy",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/bedpop.jpeg",
     "like_count": "4",
     "date_added": "2024-03-01T12:00:00Z",
     "songs": [
@@ -11,55 +11,56 @@ const data = [
       { "title": "Sofia", "artist": "Clairo", "album": "Immunity", "time": "3:09", "song_art": "assets/img/song.png" },
       { "title": "Amoeba", "artist": "Clairo", "album": "Sling", "time": "3:42", "song_art": "assets/img/song.png" },
       { "title": "Pretty Girl", "artist": "Clairo", "album": "diary 001", "time": "2:58", "song_art": "assets/img/song.png" },
-      { "title": "Your Best American Girl", "artist": "Mitski", "album": "Puberty 2", "time": "3:33", "song_art": "assets/img/song.png" },
-      { "title": "Home", "artist": "Cavetown", "album": "Cavetown", "time": "4:10", "song_art": "assets/img/song.png" },
-      { "title": "Japanese Denim", "artist": "Daniel Caesar", "album": "Pilgrim's Paradise", "time": "4:18", "song_art": "assets/img/song.png" },
     ]
   },
   {
     "playlistID": 2,
     "playlist_name": "Late Night Drive",
     "playlist_author": "Batman",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/latenight.jpg",
     "like_count": "0",
     "date_added": "2024-04-15T09:00:00Z",
     "songs": [
       { "title": "Hush", "artist": "The Marías", "album": "CINEMA", "time": "3:39", "song_art": "assets/img/song.png" },
       { "title": "Cariño", "artist": "The Marías", "album": "CINEMA", "time": "4:12", "song_art": "assets/img/song.png" },
-      { "title": "I Don't Know You", "artist": "The Marías", "album": "Superclean", "time": "3:32", "song_art": "assets/img/song.png" }
+      { "title": "I Don't Know You", "artist": "The Marías", "album": "Superclean", "time": "3:32", "song_art": "assets/img/song.png" },
+      { "title": "Your Best American Girl", "artist": "Mitski", "album": "Puberty 2", "time": "3:33", "song_art": "assets/img/song.png" },
     ]
   },
   {
     "playlistID": 3,
     "playlist_name": "Comic Crush",
-    "playlist_author": "Starfire",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_author": "Invincible",
+    "playlist_art": "assets/img/comiccrush.jpg",
     "like_count": "12",
     "date_added": "2024-04-10T14:30:00Z",
     "songs": [
       { "title": "Feeling Lonely", "artist": "boy pablo", "album": "Soy Pablo", "time": "2:42", "song_art": "assets/img/song.png" },
       { "title": "Everytime", "artist": "boy pablo", "album": "Roy Pablo", "time": "2:45", "song_art": "assets/img/song.png" },
-      { "title": "hey girl", "artist": "boy pablo", "album": "Wachito Rico", "time": "4:07", "song_art": "assets/img/song.png" }
+      { "title": "hey girl", "artist": "boy pablo", "album": "Wachito Rico", "time": "4:07", "song_art": "assets/img/song.png" },
+      { "title": "Home", "artist": "Cavetown", "album": "Cavetown", "time": "4:10", "song_art": "assets/img/song.png" },
     ]
   },
   {
     "playlistID": 4,
     "playlist_name": "Chill Indie",
-    "playlist_author": "Invincible",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_author": "Starfire",
+    "playlist_art": "assets/img/indie.jpg",
     "like_count": "8",
     "date_added": "2024-05-02T11:20:00Z",
     "songs": [
       { "title": "Feel it", "artist": "D4vd", "album": "Withered", "time": "4:03", "song_art": "assets/img/song.png" },
       { "title": "Keeping Tabs", "artist": "Cuco", "album": "Para Mi", "time": "3:30", "song_art": "assets/img/song.png" },
-      { "title": "Lover Is a Day", "artist": "Cuco", "album": "Songs4u", "time": "7:13", "song_art": "assets/img/song.png" }
+      { "title": "Lover Is a Day", "artist": "Cuco", "album": "Songs4u", "time": "7:13", "song_art": "assets/img/song.png" },
+      { "title": "Japanese Denim", "artist": "Daniel Caesar", "album": "Pilgrim's Paradise", "time": "4:18", "song_art": "assets/img/song.png" },
+
     ]
   },
   {
     "playlistID": 5,
     "playlist_name": "Lazy Days",
     "playlist_author": "Spiderman",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/lazy.jpg",
     "like_count": "19",
     "date_added": "2024-05-12T08:45:00Z",
     "songs": [
@@ -72,7 +73,7 @@ const data = [
     "playlistID": 6,
     "playlist_name": "Color Pop",
     "playlist_author": "Atom Eve",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/colorpop.jpg",
     "like_count": "54",
     "date_added": "2024-06-01T10:00:00Z",
     "songs": [
@@ -85,7 +86,7 @@ const data = [
     "playlistID": 7,
     "playlist_name": "Sunset Arcade",
     "playlist_author": "RexSplode",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/arcade.png",
     "like_count": "5",
     "date_added": "2024-03-20T16:15:00Z",
     "songs": [
@@ -98,7 +99,7 @@ const data = [
     "playlistID": 8,
     "playlist_name": "Spider-Verse Vibes",
     "playlist_author": "Miles Morales",
-    "playlist_art": "assets/img/playlist.png",
+    "playlist_art": "assets/img/spiderman.jpg",
     "like_count": "6",
     "date_added": "2024-03-27T16:15:00Z",
     "songs": [
